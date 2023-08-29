1cb6b194733d578418e59e63ab623b7fe43162f3
tommi20128 <70171019+tommi20128@users.noreply.github.com>
2023-08-29 18:34:16 +0300

    currencyCalculator tehty

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [euros, setEuros] = useState('')
+  const [krona, setKrona] = useState(0)
+
+  const calculate = () => {
+    const result = euros.replace(',','.') * 11.85
+    setKrona(result)
+  }
+
+  /* function calculate() {
+
+  } */
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.field}>Euros</Text>
+      <TextInput 
+        placeholder='Enter amount of euros' 
+        keyboardType='decimal-pad'
+        value={euros}
+        onChangeText={text => setEuros(text)}
+        style={styles.field}
+      />
+      <Text style={styles.field}>Swedish Krona</Text>
+      <Text style={styles.field}>{krona.toFixed(2)}</Text>
+      <Button title='Calculate' onPress={calculate}/>
     </View>
   );
 }
@@ -14,7 +35,11 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: 50,
+    marginLeft: 10,
+    marginRight: 10,
   },
+  field:{
+    marginBottom: 10,
+  }
 });
